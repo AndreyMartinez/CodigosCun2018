b71611a9e5aafdb884dc3587151f692974d91961
root <root@localhost.localdomain>
2018-05-21 21:52:41 -0400

    S.O proyect

---
+++ SistemasOperativos/Readme.md
@@ -0,0 +1,3 @@
+<h1>Pagina para tomar iconos</h1>
+<hr>
+<p>http://bootstrapmaster.com/live/real-static/icons-simple-line-icons.html</p>
---
+++ SistemasOperativos/public/index.js
@@ -0,0 +1,4 @@
+function desplegable(){
+    console.log("sss")
+    document.getElementById("sidebar-wrapper").classList.toggle("show");
+}
